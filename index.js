ed75de8f1edede65dd6b722c6eb5bd953f82270e
breezy-karma-0220 <lh62594@yahoo.com>
2018-11-14 19:42:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign(object, {[key]: value})  
+  return Object.assign({}, object, {[key]: value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
